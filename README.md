d070fc6f5b95bcdc027bb5cd07069bd20606fbb1
Erik MK <erik.kiebe@dentsu.com>
2021-01-20 19:32:49 +0100

    update

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 documentation and script for my server deployments
 
 ### Download from Github and pipe to bash
-wget -O - https://raw.githubusercontent.com/<username>/<project>/<branch>/<path>/<file> | bash
+`wget -O - https://raw.githubusercontent.com/<username>/<project>/<branch>/<path>/<file> | bash`
 
 #### Debian
 `wget -O - https://raw.githubusercontent.com/codatr0n/server_deployment/main/debian_docker.sh | bash`
--- debian_docker.sh
+++ debian_docker.sh
@@ -3,7 +3,7 @@ sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent softwa
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install docker-ce docker-ce-cli containerd.io -f -y
 
 # create folders for docker containers
 mkdir -v /mnt/docker
