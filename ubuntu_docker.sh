fec61fc4a9e736efbdcfa82a839d3fb1ac298b08
Erik Moeiniche Kiebe (63037075) <erik.kiebe@dentsu.com>
2021-03-02 15:12:17 +0100

    updated from portainer 1.24 to 2.x

--- ubuntu_docker.sh
+++ ubuntu_docker.sh
@@ -18,4 +18,4 @@ sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
 # install portainer
 cd ~/
 docker volume create portainer_data
-docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+docker run -d -p 8000:8000 -p 9000:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
